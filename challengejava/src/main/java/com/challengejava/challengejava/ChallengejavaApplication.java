89feaa2222e43827fa1a0526db1b699309534bbe
damianbenitez89 <78812666+damianbenitez89@users.noreply.github.com>
2021-12-01 13:07:37 -0300

    inicio del proyecto

---
+++ challengejava/src/main/java/com/challengejava/challengejava/ChallengejavaApplication.java
@@ -0,0 +1,13 @@
+package com.challengejava.challengejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengejavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengejavaApplication.class, args);
+	}
+
+}
---
+++ challengejava/src/test/java/com/challengejava/challengejava/ChallengejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challengejava.challengejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
